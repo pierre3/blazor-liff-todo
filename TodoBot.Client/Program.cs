32a016bb43955c7542d748d797e4caf40fb0ee06
Hirotada Kobayashi <pierre3@outlook.com>
2020-04-11 14:45:52 +0900

    rename Todo.Client to TodoBot.Client

--- TodoBot.Client/Program.cs
+++ TodoBot.Client/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Todo.Client
+namespace TodoBot.Client
 {
     public class Program
     {
