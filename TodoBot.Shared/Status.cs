ec9fb3e99db71ccb59d9dac0c3d09c9ba2191c48
Hirotada Kobayashi <pierre3@outlook.com>
2020-04-16 19:32:27 +0900

    add todo api server

---
+++ TodoBot.Shared/Status.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+namespace TodoBot.Shared
+{
+    /// <summary>
+    /// Todoステータス
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter), new object[] { true })]
+    public enum Status
+    {
+        /// <summary>
+        /// 準備中
+        /// </summary>
+        Ready,
+        /// <summary>
+        /// 実施中
+        /// </summary>
+        Doing,
+        /// <summary>
+        /// 実施済み(完了)
+        /// </summary>
+        Done,
+        /// <summary>
+        /// 取り消し済み
+        /// </summary>
+        Canceled
+    }
+}
---
+++ TodoBot.Shared/Todo.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace TodoBot.Shared
+{
+    /// <summary>
+    /// Todo Model
+    /// </summary>
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class Todo
+    {
+        /// <summary>
+        /// レコードID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// ユーザーID。LIFFクライアントから取得したLINEに紐づくユーザーID
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// タイトル
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// ステータス
+        /// </summary>
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// 期日
+        /// </summary>
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        public DateTime DueDate { get; set; }
+
+        public Todo()
+        {
+
+        }
+
+    }
+}
